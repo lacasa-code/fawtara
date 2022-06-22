eca05c5545b97655e06634bc73fee3cc27ca8c93
نورهان طاهر <norhan@lacasacode.come>
2022-06-22 12:31:32 +0200

    update

--- resources/views/customer/car.blade.php
+++ resources/views/customer/car.blade.php
@@ -79,7 +79,7 @@ input[type=number] {
 									<label class="control-label col-md-4 col-sm-4 col-xs-12" for="manufacturing_date">{{ trans('app.Date Of Manufacturing:') }} <label class="color-danger">*</label> </label>
 									<div class="col-md-8 col-sm-8 col-xs-12">
 									
-                                    <input type="text"  name="manufacturing_date" value="{{old('manufacturing_date')}}" placeholder="{{ trans('app.Enter Manufacturing Date')}}"  class="form-control datetimepicker" >
+                                    <input type="text"  name="manufacturing_date" value="{{old('manufacturing_date')}}" placeholder="<?php echo getDatepicker();?>"  class="form-control datetimepicker" onkeypress="return false;" required >
 									
                                     @if ($errors->has('manufacturing_date'))
 									   <span class="help-block">
