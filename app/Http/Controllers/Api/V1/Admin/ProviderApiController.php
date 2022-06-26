c9ddfe48412324eadaf00a2212fb803e24bec001
alaa mohamed <alaa.mohamed@syaaraat.com>
2022-06-27 00:43:37 +0200

    add api login

--- app/Http/Controllers/Api/V1/Admin/ProviderApiController.php
+++ app/Http/Controllers/Api/V1/Admin/ProviderApiController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers\Api\V1\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Contracts\Providers\Auth;
+use App\Helpers\Helper;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 class ProviderApiController extends Controller
 {
     public function transactaions(Request $request)
@@ -695,5 +702,83 @@ class ProviderApiController extends Controller
     		'code' => 200,
     	], 200);
     	//return $all;
+	}
+	
+
+
+	public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+        if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = auth()->user();
+        }
+        if ($validator->fails()) 
+        {
+            return response()->json(['status'=>false,'message'=>$validator->errors(),'code'=>400],400);
+        }
+        if (! $token = auth()->attempt($validator->validated()))
+        {
+            return response()->json(
+                ['status'=>false,
+                'message'=>trans('app.log_in_error'),
+                'code'=>401],401);
+        }
+        
+        $user = auth()->user();
+        $user->last_login=Carbon::now();
+        return $this->createNewToken($token);
+
     }
+
+    public function logout() 
+    {
+        auth()->logout();
+        return response()->json(['status'=>true,'message'=>trans('app.logout_success'),'code'=>200],200);
+    }
+
+    public function refresh() 
+    {
+        
+        return $this->createNewToken(auth()->refresh());
+    
+    }
+
+    protected function createNewToken($token)
+    {
+        return response()->json([
+            'status'=>true,
+            'message'=>trans('app.token_success'),
+            'code'=>201,
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'data' => auth()->user()
+        ],201);
+    }
+
+    public function userProfile()
+    {
+        return response()->json([
+            'status'=>true,
+            'message'=>trans('app.userProfile'),
+            'code'=>200,
+            'data' => auth()->user()           
+        ],200);
+    }
+
+    public function isValidToken(Request $request)
+    {
+        
+            return response()->json([
+                'status'=>true,
+                'message'=>trans('app.valid'),
+                'code'=>200,
+                'data' => auth()->check()           
+            ],200); 
+
+    }
+
 }
