21249b294b9ec571f353e6326ec8a6a58eb27b6a
alaa mohamed <alaa.mohamed@syaaraat.com>
2022-06-27 00:10:34 +0200

    add api login

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
@@ -16,3 +16,14 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], 
     Route::get('/v', 'InvoiceController@get_jobcard_no')->name('admin.get_jobcard_no');
 });
 
+*/
+
+use App\Http\Controllers\Auth\VerifyEmailController;
+
+
+//auth routes
+Route::middleware([ 'api'])->group(function () {
+
+    Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+});
