74c32825e5c354636c6047160a368f7974fcf646
نورهان طاهر <norhan@lacasacode.come>
2022-06-27 13:36:00 +0200

    update

--- resources/views/customer/list.blade.php
+++ resources/views/customer/list.blade.php
@@ -108,7 +108,6 @@
 
 <!-- Scripts starting -->
 <script src="{{ URL::asset('vendors/jquery/dist/jquery.min.js') }}"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css" rel="stylesheet">
