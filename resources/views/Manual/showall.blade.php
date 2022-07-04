8c556f15a953dac6a115b92c2aefd26988da422e
alaa mohamed <alaa.mohamed@syaaraat.com>
2022-07-04 19:23:30 +0200

    update code

--- resources/views/Manual/showall.blade.php
+++ resources/views/Manual/showall.blade.php
@@ -164,12 +164,9 @@
             	</div>
           	</div>
         </div>
-<!-- /page content
+<!-- /page content -->
 	<script src="https://code.jquery.com/jquery-1.12.3.js"></script>
-    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script> -->
-
-	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
     <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
@@ -187,7 +184,7 @@ var minDate, maxDate;
 	 function( settings, data, dataIndex ) {
 		 var min = minDate.val();
 		 var max = maxDate.val();
-		 var date = new Date( data[9] );
+		 var date = new Date(data[9]);
   
 		 if (
 			 ( min === null && max === null ) ||
