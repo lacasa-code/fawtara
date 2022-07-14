1b1086b592646082462f652d7d36f4d775ad078c
نورهان طاهر <norhan@lacasacode.come>
2022-07-14 10:07:47 +0200

    update

--- resources/views/Manual/report.blade.php
+++ resources/views/Manual/report.blade.php
@@ -106,6 +106,13 @@
             	</div>
           	</div>
         </div>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="{{ URL::asset('vendors/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('vendors/moment/min/moment.min.js') }}"></script>
+<script src="{{ URL::asset('vendors/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+<script src="{{ URL::asset('vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') }}"></script>
+
 <script type="text/javascript">
  $(document).ready(function() {
 
