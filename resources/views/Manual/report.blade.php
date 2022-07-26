0cba2f85d0ee6481bcbdff4f4f9253babe66cce3
نورهان طاهر <norhan@lacasacode.come>
2022-07-26 15:53:51 +0200

    update

--- resources/views/Manual/report.blade.php
+++ resources/views/Manual/report.blade.php
@@ -120,7 +120,7 @@
 
 										<div class="col-6">
 										    Total paid amounts :
-											<span   style="font-size: 20px;">$total_amounts </span>
+											<span   style="font-size: 20px;">$total_amounts->total_paid_amount </span>
 										</div>
                                 </div>
 								 
