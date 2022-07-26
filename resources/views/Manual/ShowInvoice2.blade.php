516aea4ebe6a116fb58c231bd459e22d18e1b0c1
نورهان طاهر <norhan@lacasacode.come>
2022-07-26 17:08:25 +0200

    update

--- resources/views/Manual/ShowInvoice2.blade.php
+++ resources/views/Manual/ShowInvoice2.blade.php
@@ -106,16 +106,18 @@
                                     <thead>
                                     </thead>
                                     <tbody>
-                                          <tr>
-                                          <td style="text-align: left;">  
+                                    <tr>
+                              <td style="text-align: left;">  
                                                       <img style="height: 70px; width: 200px;" src="{{ URL::asset('public/img/branch/marcedess-logo.svg') }}" class="cimg"> &nbsp; &nbsp; &nbsp; 
                               </td>
                               <td style="text-align: center;">  
                                     <div  >Mercedes -Benz- Almeshri Authorized Dealer </div>&nbsp; &nbsp; &nbsp; 
                               </td>
-                              <td style="text-align: right;>  
+                              <td style="text-align: right;">  
                                                       <img style="height: 70px; width: 200px;" src="{{ URL::asset('public/img/branch/'.$branch->branch_image) }}" class="cimg"> &nbsp; &nbsp; &nbsp; 
-                              </td> 
+                              </td>
+                              
+                                          </tr> 
                                           <br>
                                                              
                                                          
